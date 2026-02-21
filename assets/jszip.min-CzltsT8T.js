77f44483e075eaa372da26c17ab6a65aa95ba664
vividbooks <vitek@vividbooks.com>
2026-02-21 23:42:56 +0100

    Updates

--- assets/jszip.min-CzltsT8T.js
+++ assets/jszip.min-CzltsT8T.js
@@ -1,4 +1,4 @@
-import{be as bt,bf as It}from"./index-ChFDGZ-G.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
+import{be as bt,bf as It}from"./index-C69KFWHp.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
