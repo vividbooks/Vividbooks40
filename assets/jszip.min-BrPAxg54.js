8470897517b1e3faa33c8b41b728db02ee2212d0
vividbooks <103643334+vividbooks@users.noreply.github.com>
2026-03-23 23:18:17 +0000

    Deploying to gh-pages from @ vividbooks/Vividbooks40@75c3992d79bd16517f3b7565a993fbecb1f05a2a 🚀

--- assets/jszip.min-BrPAxg54.js
+++ assets/jszip.min-BrPAxg54.js
@@ -1,4 +1,4 @@
-import{d9 as bt,da as It}from"./index-BzzP4_mS.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
+import{d9 as bt,da as It}from"./index-BJhUQlzE.js";function vt(yt){throw new Error('Could not dynamically require "'+yt+'". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.')}var kt={exports:{}};/*!
 
 JSZip v3.10.1 - A JavaScript class for generating and reading zip files
 <http://stuartk.com/jszip>
